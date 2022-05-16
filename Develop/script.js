34a675fb4c85a82ecc5ef91b0603ea6e2ace5843
fuaus <kotingfunk@gmail.com>
2022-05-16 18:59:27 -0400

    add conditional statenment

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,9 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var lowercase = "abcedefghijklmnopqrstuvxyz"
+var uppercase = lowercase.toUpperCase()
+var number = "0123456789"
+var special = "!@#$%^&*()"
 
 // Write password to the #password input
 function writePassword() {
@@ -10,8 +14,32 @@ function writePassword() {
 
 }
 
-function generatePassrod() {
+function generatePassword() {
   var size = prompt("Please enter 8 to 128 characters")
+  console.log(size)
+  // then do validation
+  if (size >= 8 && size <= 128) {
+    var isLowerCase = confirm("include lowercase?")
+    console.log(isLowerCase)
+
+    var isUpperCase = confirm("include uppercase?")
+    console.log(isUpperCase)
+
+    var isNumber = confirm("include number?")
+    console.log(isLowerCase)
+
+    var isSpecial = confirm("include special?")
+    console.log(isSpecial)
+
+    if (isLowerCase===true) {
+      var positionString = Math.floor(Math.random()*26)
+      console.log()
+    }
+  } else {
+    alert("invalid entry")
+  }
+
+
 
 }
 
